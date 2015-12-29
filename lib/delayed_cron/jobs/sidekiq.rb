a5f2ac8d41ab3d22a4c31b9dd376e737d2e10a0c
Justin Grubbs <justin@jgrubbs.net>
2015-12-29 17:51:01 -0500

    ensure sidekiq does not schedule jobs that are enqueued, scheduled, or retrying

--- lib/delayed_cron/jobs/sidekiq.rb
+++ lib/delayed_cron/jobs/sidekiq.rb
@@ -8,19 +8,38 @@ module DelayedCron
       sidekiq_options :queue => :cron_job
 
       def self.enqueue_delayed_cron(klass, method_name, options)
-        unless scheduled?(klass, method_name)
+        unless do_not_enqueue?(klass, method_name)
           options.symbolize_keys!
           perform_in(options[:interval], klass, method_name, options)
         end
       end
 
-      def self.scheduled?(klass, method_name)
-        items = []
-        ::Sidekiq::ScheduledSet.new.each { |job| items << job.item }
-        items.select do |item|
-          item["args"][0] == klass &&
-          item["args"][1] == method_name.to_s
+      def self.do_not_enqueue?(klass, method_name)
+        scheduled?(klass, method_name) ||
+        enqueued?(klass, method_name)  ||
+        retrying?(klass, method_name)
+      end
+
+      def self.retrying?(klass, method_name)
+        ::Sidekiq::RetrySet.new.collect(&:item).select do |item|
+          matches_kass_and_method?(item, klass, method_name)
         end.present?
+      end
+
+      def self.scheduled?(klass, method_name)
+        ::Sidekiq::ScheduledSet.new.collect(&:item).select do |item|
+          matches_kass_and_method?(item, klass, method_name)
+        end.present?
+      end
+
+      def self.enqueued?(klass, method_name)
+        ::Sidekiq::Queue.new("cron_job").collect(&:item).select do |item|
+          matches_kass_and_method?(item, klass, method_name)
+        end.present?
+      end
+
+      def self.matches_kass_and_method?(item, klass, method_name)
+        item["args"][0] == klass && item["args"][1] == method_name.to_s
       end
 
       def perform(klass, method_name, options)
